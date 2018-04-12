d20dc790d11b3d87644c35d48c7edecff6001551
Amin Rahimian <rahimian@mit.edu>
2018-04-12 10:16:08 -0400

    simple bug fix in self.params['product_features']['feature'] = [20]

--- single_product_analysis/models_single_product.py
+++ single_product_analysis/models_single_product.py
@@ -30,7 +30,7 @@ class product():
             self.params['price'] = 10
         if 'product_features' not in self.fixed_params:
             self.params['product_features'] = dict.fromkeys(['feature'])
-            self.params['product_features']['feature'] = [1, 2, 3, 4]
+            self.params['product_features']['feature'] = [20]
         if 'neutral_quality' not in self.fixed_params:
             self.params['neutral_quality'] = 3
         if 'quality_std' not in self.fixed_params:
